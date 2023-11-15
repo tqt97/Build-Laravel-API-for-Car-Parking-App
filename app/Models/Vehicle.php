a39b9793e7232195d8befbd56fcc76b9ebf281a5
tqt97 <kutuanonline199@gmail.com>
2023-11-15 09:11:36 +0700

    DB Schema and Functionality

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Vehicle extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['user_id', 'plate_number'];
+}
