c67157ad04d00fad665ec736034b74aa81558b0a
tqt97 <kutuanonline199@gmail.com>
2023-11-16 16:03:08 +0700

    Get Parking Zones

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ZoneController;
 use App\Http\Controllers\Api\V1\VehicleController;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\LogoutController;
@@ -35,3 +36,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('auth/register', RegisterController::class);
 Route::post('auth/login', LoginController::class);
+
+Route::get('zones', [ZoneController::class, 'index']);
